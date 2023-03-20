657ceb976571a3d6168bfe00f54414e0cdfc3902
Abhishekgupta011 <ag25061999@gmail.com>
2023-03-20 19:28:40 +0530

    React Work

---
+++ src/component/Expenseitem.js
@@ -0,0 +1,15 @@
+function Expenseitem(){
+    const styles = {
+        lineHeight: '0.5em',
+    };
+
+    return (
+        <div>
+            <p style={styles}>Expense item</p>
+            <p style={styles}>Food Rs 10</p>
+            <p style={styles}>Petrol Rs 100</p>
+            <p style={styles}>Movies Rs 200</p>
+        </div>
+    )
+}
+export default Expenseitem;
